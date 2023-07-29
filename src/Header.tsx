adf9fc2480ae0e0fee5e3da5769991b7a6cb94f6
Jiří Matějka <jirka@jirimatejka.net>
2023-07-29 18:51:48 +0200

    First mock of login page

--- src/Header.tsx
+++ src/Header.tsx
@@ -70,34 +70,34 @@ export function Header() {
               </HStack>
             </HStack>
             <Flex alignItems="center">
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rounded="full"
-                  variant="link"
-                  cursor="pointer"
-                  minW={0}
-                >
-                  {isAuthenticated ? (
+              {isAuthenticated ? (
+                <Menu>
+                  <MenuButton
+                    as={Button}
+                    rounded="full"
+                    variant="link"
+                    cursor="pointer"
+                    minW={0}
+                  >
                     <Avatar
                       size="sm"
                       src={
                         'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
                       }
                     />
-                  ) : (
-                    <Link as={ReactRouterLink} to={getLoginRoute()}>
-                      Login
-                    </Link>
-                  )}
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Link 1</MenuItem>
-                  <MenuItem>Link 2</MenuItem>
-                  <MenuDivider />
-                  <MenuItem>Link 3</MenuItem>
-                </MenuList>
-              </Menu>
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem>Link 1</MenuItem>
+                    <MenuItem>Link 2</MenuItem>
+                    <MenuDivider />
+                    <MenuItem>Link 3</MenuItem>
+                  </MenuList>
+                </Menu>
+              ) : (
+                <Link as={ReactRouterLink} to={getLoginRoute()}>
+                  Login
+                </Link>
+              )}
             </Flex>
           </Flex>
 
---
+++ src/login/LoginPage.tsx
@@ -0,0 +1,50 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export function LoginPage() {
+  return (
+    <Flex align={'center'} justify={'center'}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={4} px={6}>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'2xl'}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={10}>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+              >
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
