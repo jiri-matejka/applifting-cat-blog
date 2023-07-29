73c34e156524bc71b2fcc2690367bacf8912d1e2
Jiří Matějka <jirka@jirimatejka.net>
2023-07-29 19:45:56 +0200

    Login page simplification

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -14,37 +14,30 @@ import {
 
 export function LoginPage() {
   return (
-    <Flex align={'center'} justify={'center'}>
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={4} px={6}>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'2xl'}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-              >
-                Sign in
-              </Button>
-            </Stack>
+    <Flex align={'center'} justify={'center'} py={4} px={6}>
+      <Box rounded={'lg'} boxShadow={'2xl'} p={8}>
+        <Stack spacing={4}>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input type="email" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Stack spacing={10}>
+            <Button
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}
+            >
+              Log in
+            </Button>
           </Stack>
-        </Box>
-      </Stack>
+        </Stack>
+      </Box>
     </Flex>
   );
 }
