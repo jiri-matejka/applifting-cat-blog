71c3728d136bdcad8740c654b0c3fece31cc09ec
Jiří Matějka <jirka@jirimatejka.net>
2023-08-02 15:46:41 +0200

    Start backend from scratch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,41 @@
-import './pre-start'; // Must be the first import
 import logger from 'jet-logger';
 
-import EnvVars from '@src/constants/EnvVars';
-import server from './server';
+import { envVariables } from '@src/constants/envVariables';
+import { loadEnv } from './loadEnv';
 
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import path from 'path';
+import helmet from 'helmet';
+import express, { Request, Response, NextFunction } from 'express';
+import logger from 'jet-logger';
 
-// **** Run **** //
+import 'express-async-errors';
 
-const SERVER_START_MSG = ('Express server started on port: ' + 
-  EnvVars.Port.toString());
+import BaseRouter from '@src/routes/api';
+import Paths from '@src/routes/constants/Paths';
 
-server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
+import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+
+import { NodeEnvs } from '@src/constants/misc';
+import { RouteError } from '@src/other/classes';
+
+loadEnv();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(envVariables.CookieProps.Secret));
+
+// Show routes called in console during development
+if (envVariables.NodeEnv === NodeEnvs.Dev) {
+  app.use(morgan('dev'));
+}
+
+// Security
+if (envVariables.NodeEnv === NodeEnvs.Production) {
+  app.use(helmet());
+}
+
+server.listen(envVariables.Port, () => logger.info(SERVER_START_MSG));
